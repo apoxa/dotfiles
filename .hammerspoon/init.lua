8ab964662aec19411c01c8d6b6427d071256c272
Benjamin Stier <b.stier@levigo.de>
2019-10-14 15:43:31 +0200

    hijack 'lock screen' to do 'start screensaver'

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,17 +1,19 @@
 hs.window.animationDuration = 0.1
-local hyper = {"ctrl", "alt", "cmd"}
+local hyper = {'ctrl', 'alt', 'cmd'}
 
-hs.loadSpoon("ReloadConfiguration")
+hs.loadSpoon('ReloadConfiguration')
 spoon.ReloadConfiguration:start()
 
-hs.loadSpoon("MiroWindowsManager")
+hs.loadSpoon('MiroWindowsManager')
 spoon.MiroWindowsManager:bindHotkeys({
-  up = {hyper, "k"},
-  right = {hyper, "l"},
-  down = {hyper, "j"},
-  left = {hyper, "h"},
-  fullscreen = {hyper, "f"}
+  up         = {hyper, 'k'},
+  right      = {hyper, 'l'},
+  down       = {hyper, 'j'},
+  left       = {hyper, 'h'},
+  fullscreen = {hyper, 'f'}
 })
+
+hs.hotkey.bind({'ctrl','cmd'}, 'q', function() hs.caffeinate.startScreensaver() end )
 
 hs.hotkey.bind({'ctrl','cmd'},'t', function()
     if hs.application.title(hs.application.frontmostApplication()) ~= 'iTerm2' then
