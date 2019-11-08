e80c9cadbbd85c34211c555ef5f794a55ea743c1
Benjamin Stier <b.stier@levigo.de>
2019-11-08 15:35:20 +0100

    update hammerspoon configuration

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,16 +1,41 @@
-hs.window.animationDuration = 0.1
+hs.loadSpoon('SpoonInstall')
+Install=spoon.SpoonInstall
+
 local hyper = {'ctrl', 'alt', 'cmd'}
+local shift_hyper = {'shift', 'ctrl', 'alt', 'cmd'}
+hs.window.animationDuration = 0.1
 
-hs.loadSpoon('ReloadConfiguration')
-spoon.ReloadConfiguration:start()
+Install:andUse('ReloadConfiguration');
 
-hs.loadSpoon('MiroWindowsManager')
-spoon.MiroWindowsManager:bindHotkeys({
-  up         = {hyper, 'k'},
-  right      = {hyper, 'l'},
-  down       = {hyper, 'j'},
-  left       = {hyper, 'h'},
-  fullscreen = {hyper, 'f'}
+Install:andUse('BrewInfo',{
+  config = {
+    brew_info_style = {
+      textFont = 'Hack Nerd Font Mono',
+      textSize = 14,
+      radius = 10 }
+    },
+    hotkeys = {
+      -- brew info
+      show_brew_info = {hyper, 'b'},
+      open_brew_url = {shift_hyper, 'b'},
+      -- brew cask info
+      show_brew_cask_info = {hyper, 'c'},
+      open_brew_cask_url = {shift_hyper, 'c'},
+    }
+  }
+)
+
+Install:andUse('MiroWindowsManager',{
+    {},
+    fn = function(m)
+        m:bindHotkeys({
+        up         = {hyper, 'k'},
+        right      = {hyper, 'l'},
+        down       = {hyper, 'j'},
+        left       = {hyper, 'h'},
+        fullscreen = {hyper, 'f'}
+        })
+    end
 })
 
 hs.hotkey.bind({'ctrl','cmd'}, 'q', function() hs.caffeinate.startScreensaver() end )
@@ -24,6 +49,7 @@ end)
 ht = hs.loadSpoon("HammerText")
 ht.keywords = {
     ["..shrug"] = "¯\\_(ツ)_/¯",
+    ["..eshrug"] = "¯\\\\_(ツ)_/¯",
     ["..tf"] = "(╯°□°）╯︵ ┻━┻",
     ["..paste"] = function() return hs.pasteboard.getContents() end,
 }
