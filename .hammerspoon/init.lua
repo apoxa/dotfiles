d097ee09e408a0abeede0111adc890b6f7fde857
Benjamin Stier <b.stier@levigo.de>
2020-04-02 12:10:55 +0200

    replaced hammerspoon text with alfred

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,24 +7,6 @@ hs.window.animationDuration = 0.1
 
 Install:andUse('ReloadConfiguration');
 
-Install:andUse('BrewInfo',{
-  config = {
-    brew_info_style = {
-      textFont = 'Hack Nerd Font Mono',
-      textSize = 14,
-      radius = 10 }
-    },
-    hotkeys = {
-      -- brew info
-      show_brew_info = {hyper, 'b'},
-      open_brew_url = {shift_hyper, 'b'},
-      -- brew cask info
-      show_brew_cask_info = {hyper, 'c'},
-      open_brew_cask_url = {shift_hyper, 'c'},
-    }
-  }
-)
-
 Install:andUse('MiroWindowsManager',{
     {},
     fn = function(m)
@@ -44,13 +26,3 @@ hs.hotkey.bind({'ctrl','cmd'},'t', function()
     term=hs.application.find('iTerm2') or hs.application.open('iTerm')
     term:selectMenuItem('New Window')
 end)
-
-
-ht = hs.loadSpoon("HammerText")
-ht.keywords = {
-    ["..shrug"] = "¯\\_(ツ)_/¯",
-    ["..eshrug"] = "¯\\\\_(ツ)_/¯",
-    ["..tf"] = "(╯°□°）╯︵ ┻━┻",
-    ["..paste"] = function() return hs.pasteboard.getContents() end,
-}
-ht:start()
