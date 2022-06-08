cd6e0d8f27378453ae6e0fb5540e73f64629aefe
Benjamin Stier <b.stier@levigo.de>
2022-06-08 13:13:53 +0200

    Add fzf configuration to pwsh.

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,7 @@
 Set-PSReadlineOption -EditMode vi
 Set-PSReadLineKeyHandler -Chord ctrl+w -Function BackwardDeleteWord
+
+# fzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Set-PsFzfOption -TabExpansion
