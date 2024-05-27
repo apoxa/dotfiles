2436437ba83c5cd3eef8883388615fe71491829c
Benjamin Stier <b.stier@levigo.de>
2024-05-27 13:19:23 +0200

    luaformat.

--- .config/yadm/alt/.hammerspoon##os.Darwin/init.lua
+++ .config/yadm/alt/.hammerspoon##os.Darwin/init.lua
@@ -9,101 +9,107 @@ windowCornerRadius = 10
 --# bindings
 
 --# reload config
-hs.hotkey.bind(super, hs.keycodes.map["return"], nil, function() hs.reload() end)
+hs.hotkey.bind(super, hs.keycodes.map["return"], nil, function()
+	hs.reload()
+end)
 
+hs.loadSpoon("SpoonInstall")
+Install = spoon.SpoonInstall
 
-hs.loadSpoon('SpoonInstall')
-Install=spoon.SpoonInstall
-
-local hyper = {'ctrl', 'alt', 'cmd'}
-local shift_hyper = {'shift', 'ctrl', 'alt', 'cmd'}
+local hyper = { "ctrl", "alt", "cmd" }
+local shift_hyper = { "shift", "ctrl", "alt", "cmd" }
 hs.window.animationDuration = 0.0
 hs.application.enableSpotlightForNameSearches(false)
 
-hs.hotkey.bind({'ctrl','cmd'}, 'q', function() hs.caffeinate.startScreensaver() end )
-
-hs.hotkey.bind({'ctrl','cmd'},'t', function()
-    term=hs.application.find('iTerm2') or hs.application.open('iTerm')
-    term:selectMenuItem('New Window')
+hs.hotkey.bind({ "ctrl", "cmd" }, "q", function()
+	hs.caffeinate.startScreensaver()
 end)
 
-hs.hotkey.bind(shift_hyper, 'n', function() hs.eventtap.keyStroke({}, 71) end)
+hs.hotkey.bind({ "ctrl", "cmd" }, "t", function()
+	term = hs.application.find("iTerm2") or hs.application.open("iTerm")
+	term:selectMenuItem("New Window")
+end)
+
+hs.hotkey.bind(shift_hyper, "n", function()
+	hs.eventtap.keyStroke({}, 71)
+end)
 
 -- Register my custom repository
 Install.repos["apoSpoons"] = {
-  desc   = "apoxa's repository",
-  url    = "https://github.com/apoxa/apoSpoons",
-  branch = 'main'
+	desc = "apoxa's repository",
+	url = "https://github.com/apoxa/apoSpoons",
+	branch = "main",
 }
 
 hs.loadSpoon("GlobalMute")
 spoon.GlobalMute:bindHotkeys({
-    toggle = {hyper, "F12"}
+	toggle = { hyper, "F12" },
 })
 
 function appID(app)
-    infoBundle = hs.application.infoForBundlePath(app)
-    if infoBundle ~= nil then
-        return infoBundle['CFBundleIdentifier']
-    end
+	infoBundle = hs.application.infoForBundlePath(app)
+	if infoBundle ~= nil then
+		return infoBundle["CFBundleIdentifier"]
+	end
 end
 
-chromeBrowser = appID('/Applications/Google Chrome.app')
-edgeBrowser = appID('/Applications/Microsoft Edge.app')
-firefoxBrowser = appID('/Applications/Firefox.app')
-teamsApp = appID('/Applications/Microsoft Teams classic.app')
+chromeBrowser = appID("/Applications/Google Chrome.app")
+edgeBrowser = appID("/Applications/Microsoft Edge.app")
+firefoxBrowser = appID("/Applications/Firefox.app")
+teamsApp = appID("/Applications/Microsoft Teams classic.app")
 
 DefaultBrowser = firefoxBrowser
 
-Install:andUse("URLDispatcher",
-    {
-        config = {
-            url_patterns = {
-                { "msteams:", teamsApp },
-            },
-            url_redir_decoders = {
-                -- Send MS Teams URLs directly to the app
-                { "MS Teams URLs", "(https://teams.microsoft.com.*)", "msteams:%1", true },
-                -- Preview incorrectly encodes the anchor
-                -- character in URLs as %23, we fix it
-                { "Fix broken Preview anchor URLs", "%%23", "#", false, "Preview" },
-            },
-            default_handler = DefaultBrowser,
-        },
-        start = true,
-        -- Enable debug logging if you get unexpected behavior
-        -- loglevel = 'debug'
-    }
-)
+Install:andUse("URLDispatcher", {
+	config = {
+		url_patterns = {
+			{ "msteams:", teamsApp },
+		},
+		url_redir_decoders = {
+			-- Send MS Teams URLs directly to the app
+			{ "MS Teams URLs", "(https://teams.microsoft.com.*)", "msteams:%1", true },
+			-- Preview incorrectly encodes the anchor
+			-- character in URLs as %23, we fix it
+			{ "Fix broken Preview anchor URLs", "%%23", "#", false, "Preview" },
+		},
+		default_handler = DefaultBrowser,
+	},
+	start = true,
+	-- Enable debug logging if you get unexpected behavior
+	-- loglevel = 'debug'
+})
 
 -- global config
 config = {
-    networks = {
-        ['wlan@somewhere'] = 'levigo',
-        ['DWMC3'] = 'DWMC3'
-    }
+	networks = {
+		["wlan@somewhere"] = "levigo",
+		["DWMC3"] = "DWMC3",
+	},
 }
 
 -- requires
-watchables   = require('utils.watchables')
-controlplane = require('utils.controlplane')
-
+watchables = require("utils.watchables")
+controlplane = require("utils.controlplane")
 
 -- controlplane
-controlplane.enabled = { 'autonl' }
+controlplane.enabled = { "autonl" }
 
 -- start/stop modules
 local modules = { controlplane, watchables }
 
 hs.fnutils.each(modules, function(module)
-    if module then module.start() end
+	if module then
+		module.start()
+	end
 end)
 
 -- stop modules on shutdown
 hs.shutdownCallback = function()
-    hs.fnutils.each(modules, function(module)
-        if module then module.stop() end
-    end)
+	hs.fnutils.each(modules, function(module)
+		if module then
+			module.stop()
+		end
+	end)
 end
 
 -- axbrowse
