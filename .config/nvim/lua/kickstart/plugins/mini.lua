5e6f5b7c69d96eb1af03b5032c485ec9c23d4d6e
Benjamin Stier <b.stier@levigo.de>
2024-07-24 16:11:59 +0200

    fix(nvim): Don't show hidden buffers in tabline.

--- .config/nvim/lua/kickstart/plugins/mini.lua
+++ .config/nvim/lua/kickstart/plugins/mini.lua
@@ -34,7 +34,7 @@ return {
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
       -- tabline
-      require('mini.tabline').setup()
+      require('mini.tabline').setup { set_vim_settings = false }
 
       -- You can configure sections in the statusline by overriding their
       -- default behavior. For example, here we set the section for
