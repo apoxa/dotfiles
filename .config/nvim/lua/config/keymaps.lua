46b793db32abca4646219e624a4195b7d2e806b5
Benjamin Stier <b.stier@levigo.de>
2025-07-30 09:02:13 +0200

    nvim: Disable alt-j and alt-k keybinds.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- In tmux, esc happens to trigger alt if not waiting long enough. I don't use these keybinds anyways.
+-- See: https://github.com/LunarVim/LunarVim/issues/1857#issuecomment-2273066106
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
