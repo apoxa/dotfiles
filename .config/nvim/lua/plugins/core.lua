a806ef29826fa880f87db358c3e9ad0e4ef08939
Benjamin Stier <b.stier@levigo.de>
2025-12-22 12:37:53 +0100

    feat(neovim): Install ltex-plus-ls.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -44,12 +44,35 @@ return {
         "prettier",
         "vale",
         "cbfmt", -- format codeblocks in markdown
+        "ltex-ls-plus", -- language server for LanguageTool with support for LaTeX, Markdown, and others
       },
     },
   },
+
+  -- -- Helper for ltex-ls which provides e.g. "add to dictionary"
+  -- {
+  --   "barreiroleo/ltex_extra.nvim",
+  --   ft = { "markdown", "tex" },
+  --   dependencies = { "neovim/nvim-lspconfig" },
+  --   -- yes, you can use the opts field, just I'm showing the setup explicitly
+  --   config = function()
+  --     require("ltex_extra").setup({
+  --       {},
+  --       server_opts = {
+  --         capabilities = {},
+  --         on_attach = function(client, bufnr)
+  --           -- your on_attach process
+  --         end,
+  --         settings = {
+  --           ltex = {},
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
+
   -- set dark mode from system
   -- https://github.com/f-person/auto-dark-mode.nvim
-
   {
     "f-person/auto-dark-mode.nvim",
     opts = {
